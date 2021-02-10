032272af8cbc62f97bc1f6413b837078f4b88df9
KoenVDZ <koenatwork@gmail.com>
2021-02-10 16:59:38 -0300

    testimonials

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,40 +1,42 @@
 class Modal {
   constructor() {
-    this.injectHTML()
-    this.modal = document.querySelector(".modal")
-    this.closeIcon = document.querySelector(".modal__close")
-    this.events()
+    this.injectHTML();
+    this.modal = document.querySelector(".modal");
+    this.closeIcon = document.querySelector(".modal__close");
+    this.events();
   }
 
   events() {
     // listen for close click
-    this.closeIcon.addEventListener("click", () => this.closeTheModal())
+    this.closeIcon.addEventListener("click", () => this.closeTheModal());
 
     // pushes any key
-    document.addEventListener("keyup", e => this.keyPressHandler(e))
+    document.addEventListener("keyup", (e) => this.keyPressHandler(e));
   }
 
   keyPressHandler(e) {
     if (e.keyCode == 27) {
-      this.closeTheModal()
+      this.closeTheModal();
     }
   }
 
   openTheModal() {
-    this.modal.classList.add("modal--is-visible")
+    this.modal.classList.add("modal--is-visible");
   }
 
   closeTheModal() {
-    this.modal.classList.remove("modal--is-visible")
+    this.modal.classList.remove("modal--is-visible");
   }
 
   injectHTML() {
-    document.body.insertAdjacentHTML('beforeend', `
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
     <div class="modal">
     <div class="modal__inner">
       <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
       <div class="wrapper wrapper--narrow">
-        <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
+        <p class="modal__description">You can get in touch with me through different channels, please choose the one which suits you most, I will get back to you as soon as I can.</p>
       </div>
 
       <div class="social-icons">
@@ -46,8 +48,9 @@ class Modal {
     </div>
     <div class="modal__close">X</div>
   </div>
-    `)
+    `
+    );
   }
 }
 
-export default Modal
+export default Modal;
