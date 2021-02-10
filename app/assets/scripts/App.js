e574200a1c5ceee8db963e3d9b7508f223ac8aca
KoenVDZ <koenatwork@gmail.com>
2021-02-10 10:59:39 -0300

    services-item added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,7 +7,7 @@ import CurrentYear from './modules/CurrentYear'
 
 new CurrentYear()
 new StickyHeader()
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".services-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new MobileMenu();
 let modal
