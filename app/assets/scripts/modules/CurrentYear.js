377f6790936bc7fd98c95d9cdc51c493e4bfd83c
KoenVDZ <koenatwork@gmail.com>
2021-02-26 08:55:11 -0300

    Spelling mistakes

--- app/assets/scripts/modules/CurrentYear.js
+++ app/assets/scripts/modules/CurrentYear.js
@@ -1,6 +1,9 @@
 class CurrentYear {
   constructor() {
-    document.getElementById("site-footer__date").innerHTML = 'Copyright \u00A9' + new Date().getFullYear() + 'Tour Guide Gus. All rights reserved.'
+    document.getElementById("site-footer__date").innerHTML =
+      "Copyright &copy" +
+      new Date().getFullYear() +
+      "Tour Guide Gus. All rights reserved.";
   }
 }
-export default CurrentYear
+export default CurrentYear;
